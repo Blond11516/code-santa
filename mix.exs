ec60e2bcf4a761a03e8350c738be3135e3f1cb28
Étienne Lévesque <contact@etiennel.dev>
2023-09-20 18:52:23 -0400

    update dependencies

--- mix.exs
+++ mix.exs
@@ -23,14 +23,14 @@ defmodule CodeSanta.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:req, "0.3.1"},
-      {:floki, "0.34.0"},
-      {:jason, "1.4.0"},
+      {:req, "0.4.3"},
+      {:floki, "0.34.3"},
+      {:jason, "1.4.1"},
       {:oban, "2.13.4"},
       {:tzdata, "1.1.1"},
       {:dotenv_parser, "2.0.0", runtime: false},
-      {:credo, "1.6.7", only: :dev, runtime: false},
-      {:dialyxir, "1.2.0", only: :dev, runtime: false}
+      {:credo, "1.7.0", only: :dev, runtime: false},
+      {:dialyxir, "1.4.1", only: :dev, runtime: false}
     ]
   end
 
